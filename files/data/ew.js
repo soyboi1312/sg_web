3621dca3bdf44e767c885299e5ce8149ada10a75
Spencer Sams <spencers@gmail.com>
2025-07-21 14:06:12 -0700

    Add files via upload

---
+++ files/data/ew.js
@@ -0,0 +1,7 @@
+export const ewEnv = ["Air", "Land", "Surface", "Subsurface", "Space"];
+
+export const ewEnvInd = [
+  "J3.7 Track(EW Product Info)",
+  "J3.7 Track(EW Product Info) with J14.0 Parametric Info",
+  "J14.0 Track(EW Parametrics)",
+];
---
+++ files/data/reference.js
@@ -0,0 +1,121 @@
+export const refPoint = [
+    "Emergency",
+    "Hazard",
+    "Reference Point (General)",
+    "Station (General)",
+    "Station (Air)",
+    "Area (General)",
+    "Area (Hazard)",
+    "ASW",
+    "ASW, 1",
+];
+
+export const refAmp = [
+    "N.S.",
+    "Marshall Point",
+    "Waypoint",
+    "Corridor Tab",
+    "Position And Intended Movement (PIM)",
+    "Disposition Center",
+    "Formation Center",
+    "Search Area",
+    "Victor Lima (VL)",
+    "Submarine Position And Intended Movement (SISM)",
+];
+
+export const emergencyAmp = [
+    "N.S.",
+    "DOWNED A/C",
+    "MAN IN WATER",
+    "DITCHING",
+    "BAILOUT",
+    "DISTRESSED VESSEL",
+    "GROUND INCIDENT",
+    "MEDICAL",
+    "ISOLATED PERSON(S)",
+];
+
+export const generalStationAmp = [
+    "N.S.",
+    "Tomcat",
+    "Picket",
+    "Rendezvous",
+    "Replenishment",
+    "Rescue",
+];
+
+export const airStationAmp = [
+    "N.S.",
+    "Combat Air Patrol (CAP)",
+    "Airborne Early Warning (AEW)",
+    "Antisubmarine Warfare (ASW) Fixed Wing",
+    "Antisubmarine Warfare (ASW) Helicopter (HELO)",
+    "Replenishment",
+    "Strike Initial Point (IP)",
+    "TACAN",
+    "Tanker",
+    "Orbit, Race Track",
+    "Orbit, Figure Eight",
+    "Orbit, Random Closed",
+    "Orbit Point",
+];
+
+export const generalAreaAmp = [
+    "N.S.",
+    "Search",
+    "Restricted",
+    "Exercise",
+    "Submarine Patrol Area",
+    "Fighter Engagement Zone/Fighter AOR",
+    "Ground Area of Responsibility",
+    "Defended Area",
+];
+
+export const hazardAreaAmp = [
+    "N.S.",
+    "Danger",
+    "Contaminated",
+    "Missile Engagement Zone",
+    "Hostile Weapon Zone",
+    "Hostile Tactical Area",
+    "SHORAD",
+    "Kill Zone",
+    "Target Area Of Interest",
+    "Named Area Of Interest (NAI)",
+];
+
+export const aswAmp = [
+    "N.S.",
+    "Sinker",
+    "Brief Contact",
+    "Search Center (ASW)",
+    "Estimated Position (EP)",
+    "Fix (ASW)",
+    "Notack Area",
+    "Moving Haven",
+    "Sonobuoy Position",
+    "Sonobuoy Pattern Reference Position",
+    "Limiting Line of Approach",
+];
+
+export const asw1amp = [
+    "N.S.",
+    "Charted Wreck",
+    "Bottomed Nonsubmarine",
+    "ASW Station",
+];
+
+export const hazardAmp = [
+    "Mine",
+    "Ground Zero",
+    "Aim/Weapon Entry Point",
+    "Electronic Attack (EA) Decoy",
+    "Engagement Point",
+    "Oil Rig",
+    "N.S.",
+];
+
+export const trackIndName = [
+    "J3.4 Track(ASW)",
+    "J5.4 Track(ASW Acoustic Bearing/Range)",
+];
