cfaea24498d2d3020ec778de90618b26cf4000ea
Spencer Sams <spencers@gmail.com>
2025-07-22 08:57:24 -0700

    Delete files/data directory

--- files/data/ew.js
+++
@@ -1,7 +0,0 @@
-export const ewEnv = ["Air", "Land", "Surface", "Subsurface", "Space"];
-
-export const ewEnvInd = [
-  "J3.7 Track(EW Product Info)",
-  "J3.7 Track(EW Product Info) with J14.0 Parametric Info",
-  "J14.0 Track(EW Parametrics)",
-];
